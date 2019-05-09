b62db464b299c6dfbd760126b8f317577889b61e
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-05-09 15:54:12 -0700

    Adding post and relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  has_many :posts
+  #VALID_EMAIL_REGEX = /\A[\w+-.]+@[a-z\d-.]+.[a-z]+\z/i
+  validates :email, presence: true,  length: { maximum: 255 },
+            #format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  validates :name, presence: true,  length: { maximum: 50 }
+  validates :password, presence: true,  length: { minimum: 6 }
 end
