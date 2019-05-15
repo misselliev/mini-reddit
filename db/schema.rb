7d9487007a2025c34617a7ee69f2e8ff57e773d5
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-05-15 09:28:25 -0700

    Adding foreign key to preserve integrity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_11_212405) do
+ActiveRecord::Schema.define(version: 2019_05_15_013741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,4 +41,5 @@ ActiveRecord::Schema.define(version: 2019_05_11_212405) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "posts", "users", on_delete: :cascade
 end
