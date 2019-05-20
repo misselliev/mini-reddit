6fa320ba18a88a5ea520e349eec1bf0865a30536
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-05-20 12:09:03 -0700

    adding dependent destroy for comments in post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: { maximum: 30 }
   validates :body, presence: true, length: { maximum: 255 }
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
